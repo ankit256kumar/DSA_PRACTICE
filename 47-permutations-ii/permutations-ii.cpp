b82c2eaa46089da99ba79f7d3bb72f7ff88d8ea1
ankit256kumar <kumarankit36678@gmail.com>
2026-08-13 00:08:22 +0530

    Time: 7 ms (34.92%) | Memory: 15.1 MB (21.06%) - LeetSync

---
+++ 47-permutations-ii/permutations-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    void getPerms(vector<int>& nums, int idx, vector<vector<int>>& ans) {
+
+        if (idx == nums.size()) {
+            ans.push_back(nums);
+            return;
+        }
+
+        unordered_set<int> s;
+
+        for (int i = idx; i < nums.size(); i++) {
+
+            if (s.find(nums[i]) != s.end())
+                continue;
+
+            s.insert(nums[i]);
+
+            swap(nums[idx], nums[i]);
+
+            getPerms(nums, idx + 1, ans);
+
+            swap(nums[idx], nums[i]);   // Backtracking
+        }
+    }
+
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+
+        vector<vector<int>> ans;
+
+        getPerms(nums, 0, ans);
+
+        return ans;
+    }
+};
